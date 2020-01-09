2989c0b56825ce41abb1ad00c2c9cbd7cafa6291
David Figueroa <democres@hotmail.es>
2020-01-09 07:38:12 -0500

    Initial Commit

---
+++ build/CMakeFiles/cxxforth.cpp.md.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cxxforth.cpp.md"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cxxforth.cpp.md.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cxxforth.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cxxforth.dir/cxxforth.cpp.o"
+  "cxxforth"
+  "cxxforth.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cxxforth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
